7602d11a7333fc8f5ae0cfa05f0ad628d1c9fc9a
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2018-07-24 01:21:17 +0200

    put dropout to cnn layer

--- assignment2/nn/cnn.py
+++ assignment2/nn/cnn.py
@@ -2,7 +2,9 @@ import numpy as np
 import tensorflow as tf
 
 
-def cnn(idx, X, filters, kernel_size, is_training, strides=(1, 1, 1, 1), padding='SAME', use_batchnorm=True):
+def cnn(idx, X, filters, kernel_size, is_training,
+        strides=(1, 1, 1, 1), padding='SAME', use_batchnorm=True,
+        dropout=None):
     input_filter_size = X.shape[-1]
     Wconv = tf.get_variable(f'Wconv{idx}', shape=[*kernel_size, input_filter_size, filters])
     bconv = tf.get_variable(f'bconv{idx}', shape=[filters])
@@ -15,4 +17,8 @@ def cnn(idx, X, filters, kernel_size, is_training, strides=(1, 1, 1, 1), padding
     # axis=3 channel axis
     if use_batchnorm:
         out = tf.layers.batch_normalization(out, axis=3, training=is_training)
+
+    if dropout is not None:
+        out = tf.layers.dropout(out, rate=dropout, training=is_training)
+
     return out, [Wconv, bconv]
